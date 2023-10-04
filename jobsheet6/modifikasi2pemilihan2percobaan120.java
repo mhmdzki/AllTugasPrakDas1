27e1958f0017aafa9778604dba4e8eeb1ec0e360
mhmdzki <144131654+mhmdzki@users.noreply.github.com>
2023-10-04 09:58:19 +0700

    Add files via upload

---
+++ jobsheet6/modifikasi2pemilihan2percobaan120.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class modifikasi2pemilihan2percobaan120 {
+
+    public static void main(String[] args) {
+        Scanner input20 = new Scanner(System.in);
+        int tahun;
+        System.out.println("Masukkan Tahun");
+        tahun = input20.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0) {
+                System.out.println("Tahun Kabisat");
+             } else if ((tahun % 100) == 0) {
+            if ((tahun % 400) == 0) {
+                System.out.println("Tahun Kabisat");
+            }else
+            System.out.println("Bukan Tahun Kabisat");
+            }
+        
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
+    } 
+}
