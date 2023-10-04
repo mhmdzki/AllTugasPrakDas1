dd75c3e593ab26b1252fb7c3bafc7741fdb46a50
mhmdzki <mhmdzki00@gmail.com>
2023-10-04 08:27:03 +0700

    y

--- jobsheet6/pemilihan2percobaan120.java
+++ jobsheet6/pemilihan2percobaan120.java
@@ -10,8 +10,7 @@ public class pemilihan2percobaan120 {
 
         if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0) 
-            System.out.println("Tahun Kabisat");
-                
+            System.out.println("Tahun Kabisat :");
             }else
             System.out.println("Bukan Tahun Kabisat");
             
