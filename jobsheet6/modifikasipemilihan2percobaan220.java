9a443fe2fa9f3d43f5ca2b2c2f1a0b993c4abfa5
mhmdzki <mhmdzki00@gmail.com>
2023-10-16 17:40:21 +0700

    p6

--- jobsheet6/modifikasipemilihan2percobaan220.java
+++ jobsheet6/modifikasipemilihan2percobaan220.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 /**
  * pemilihan2percobaan220
  */
-public class pemilihan2percobaan220 {
+public class modifikasipemilihan2percobaan220 {
 
     public static void main(String[] args) {
         Scanner input20 = new Scanner(System.in);
@@ -19,10 +19,14 @@ public class pemilihan2percobaan220 {
 
         if (totalSudut == 180){
             if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90 ))
-            System.out.println("Segitiga tersebut adalah segitiga siku-siku");
-            else 
-            System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
+            System.out.println("Segitiga tersebut adalah segitiga siku-siku"); 
+            else if ((sudut1 == sudut2) && (sudut2 == sudut3)){
+            System.out.println("Segitiga tersebut adalah segitiga sama sisi");
+            } else if ((sudut1 == sudut2) || (sudut2 == sudut3)){
+                System.out.println("Segitiga tersebut adalah segitiga sama kaki");
+            }else 
+            System.out.println("Segitiga tersebut adalah segitiga sembarang");
         } else
         System.out.println("Bukan Segitiga");
- }
+    }
 }
--- jobsheet6/pemilihan2percobaan320.java
+++ jobsheet6/pemilihan2percobaan320.java
@@ -25,7 +25,7 @@ public class pemilihan2percobaan320 {
             gajiBersih = (int) (penghasilan - (pajak * penghasilan));
             System.out.print("penghasilan bersih : " + gajiBersih);
 
-        } else if (kategori.equalsIgnoreCase("pebisnis")) {
+        } else if (kategori.equals("pebisnis")) {
             if (penghasilan <= 2500000)
             pajak = 0.15;
             else if (penghasilan <= 350000)
