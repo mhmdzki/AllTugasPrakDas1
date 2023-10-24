fde41e07286f58d0cadae3fdd543bf724b56adfa
mhmdzki <mhmdzki00@gmail.com>
2023-10-24 17:39:31 +0700

    jobsheet 9

---
+++ jobsheet 9/Modifikasi2ArrayBilangan20.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * ArrayBilangan20
+ */
+public class Modifikasi2ArrayBilangan20 {
+
+    public static void main(String[] args) {
+        int bil [] = {5, 13, -7, 17};
+        
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bil[i]);
+            
+        }
+    }
+}
